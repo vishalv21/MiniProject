2ab46b15cf64df5269018737531d16bfc7d0fbdf
vishalv21 <vishal.varma21702@gmail.com>
2023-03-20 18:26:14 +0530

    Added new tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -42,4 +42,40 @@ public class CalculatorTest {
         assertNotEquals(cal_value,exp_value,delta);
 //        assertTrue( true );
     }
+
+    @Test
+    public void testPower2()
+    {
+        double cal_value=calci.power(2.0,3.0);
+        double exp_value=20;
+        assertNotEquals(cal_value,exp_value,delta);
+//        assertTrue( true );
+    }
+
+    @Test
+    public void testPower1()
+    {
+        double cal_value=calci.power(2.0,3.0);
+        double exp_value=8.0;
+        assertEquals(cal_value,exp_value,delta);
+//        assertTrue( true );
+    }
+
+    @Test
+    public void testLog2()
+    {
+        double cal_value=calci.log(1.0);
+        double exp_value=20;
+        assertNotEquals(cal_value,exp_value,delta);
+//        assertTrue( true );
+    }
+
+    @Test
+    public void testLog1()
+    {
+        double cal_value=calci.log(1.0);
+        double exp_value=0;
+        assertEquals(cal_value,exp_value,delta);
+//        assertTrue( true );
+    }
 }
